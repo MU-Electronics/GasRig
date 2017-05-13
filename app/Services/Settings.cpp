a95cb11700c3cc5926b34845fa14f5551017bdf9
motters <sammottleys@gmail.com>
2017-05-13 20:30:18 +0100

    Created a base settings class to load and update json files stored in text or binary format

--- app/Services/Settings.cpp
+++ app/Services/Settings.cpp
@@ -1,8 +1,90 @@
 #include "Settings.h"
 
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QDebug>
+
 namespace App { namespace Services
 {
-    Settings::Settings()
+    Settings::Settings(QString location, Settings::SaveFormat format)
+        : m_loadedFile(location),
+          m_format(format)
     {
+        load();
     }
+
+    Settings::~Settings()
+    {
+
+    }
+
+    /**
+     * Load the settings from a file
+     *
+     * @brief Settings::load
+     * @param location
+     * @param format
+     * @return bool
+     */
+    bool Settings::load()
+    {
+        // Load the file
+        QFile loadFile(m_loadedFile);
+
+        // Open the file and return error if failed
+        if (!loadFile.open(QIODevice::ReadOnly)) {
+            qWarning("Couldn't open save file.");
+            return false;
+        }
+
+        // Get the file data
+        QByteArray saveData = loadFile.readAll();
+
+        // Load the document into the json object from json or binary stored format
+        QJsonDocument loadDoc(m_format == Json
+            ? QJsonDocument::fromJson(saveData)
+            : QJsonDocument::fromBinaryData(saveData));
+
+        // Trigger the read method
+        read(loadDoc.object());
+
+        return true;
+    }
+
+
+    /**
+     * Update the loaded file with the new data
+     *
+     * @brief Settings::update
+     * @return bool
+     */
+    bool Settings::update() const
+    {
+        // Load the file
+        QFile saveFile(m_loadedFile);
+
+        // Open the file and error if failed
+        if (!saveFile.open(QIODevice::WriteOnly)) {
+            qWarning("Couldn't open save file.");
+            return false;
+        }
+
+        // Create a json object
+        QJsonObject object;
+
+        // Add data to the json object
+        write(object);
+
+        // Create a json format document
+        QJsonDocument saveDoc(object);
+
+        // Save the file in the correct format
+        saveFile.write(m_format == Json
+            ? saveDoc.toJson()
+            : saveDoc.toBinaryData());
+
+        return true;
+    }
+
 }}
--- app/Services/Settings.h
+++ app/Services/Settings.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include <QString>
+#include <QDebug>
+#include <QJsonObject>
+
 namespace App { namespace Services{
     class Settings
     {
     public:
-        Settings();
+        Settings(QString location, Settings::SaveFormat format);
+        ~Settings();
 
+        enum SaveFormat {
+            Json, Binary
+        };
+
+        QString m_loadedFile;
+        SaveFormat m_format;
+
+        bool load();
+        bool update() const;
+
+        virtual void read(const QJsonObject &json) = 0;
+        virtual void write(QJsonObject &json) const = 0;
     private:
         explicit Settings(const Settings& rhs) = delete;
         Settings& operator= (const Settings& rhs) = delete;
