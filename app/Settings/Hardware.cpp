8578add37389c1f8cf6a42cc376941413362af5d
Sam Mottley <sammottley@e-u07paat2312a.it.manchester.ac.uk>
2017-05-18 03:42:13 -0700

    Hardware setting manager is now fully functional including reading and writing

--- app/Settings/Hardware.cpp
+++ app/Settings/Hardware.cpp
@@ -20,29 +20,83 @@ namespace App { namespace Settings
 
     void Hardware::read(const QJsonObject &json)
     {
-        // Store usb connection info
+        // Get root to map
+        QVariantMap rootMap = json.toVariantMap();
 
+        // Store usb connection info
+        auto connectionData = rootMap["usb_connections"].toMap();
+        usb_connections["pressure_sensor"] = connectionData["pressure_sensor"].toMap();
+        usb_connections["vacuum_station"] = connectionData["vacuum_station"].toMap();
+        usb_connections["labJack"] = connectionData["labJack"].toMap();
+        usb_connections["flow_controller_one"] = connectionData["flow_controller_one"].toMap();
+        usb_connections["flow_controller_two"] = connectionData["flow_controller_two"].toMap();
 
         // Store valve connections
-
+        valve_connections = rootMap["valve_connections"].toMap();
 
         // Store absolute maxmiums
-
+        absoulte_maximums = rootMap["absoulte_maximums"].toMap();
 
         // Store polling times
-
-
-        // For debugging will be removed
-        QJsonObject test;
-        write(test);
+        polling_times = rootMap["polling_times"].toMap();
     }
 
     void Hardware::write(QJsonObject &json) const
     {
+        // Save usb connection info
+        QJsonObject usbConnectionsData;
+
+        // Loop trhough usb set congiuration
+        for(auto & outer : usb_connections.toStdMap())
+        {
+            // Cache inner value states for each error condition
+            QJsonObject usbConnectionData;
+
+            // Set the valve  states
+            for(auto & inner : outer.second.toMap().toStdMap())
+            {
+                // Get the valve id
+                QString valveId = inner.first;
+
+                // Get the valve state
+                bool state = inner.second.toBool();
+
+                // Set the states in the cache inner container
+                usbConnectionData[valveId] = state;
+            }
+
+            // Write the state of the cache inner container to end container
+            usbConnectionsData[outer.first] = usbConnectionData;
+        }
+        json["usb_connections"] = usbConnectionsData;
+
+        // Save valve connections
+        QJsonObject valveConnectionsData;
+        valveConnectionsData["1"] = valve_connections["1"].toString();
+        valveConnectionsData["2"] = valve_connections["2"].toString();
+        valveConnectionsData["3"] = valve_connections["3"].toString();
+        valveConnectionsData["4"] = valve_connections["4"].toString();
+        valveConnectionsData["5"] = valve_connections["5"].toString();
+        valveConnectionsData["6"] = valve_connections["6"].toString();
+        valveConnectionsData["7"] = valve_connections["7"].toString();
+        valveConnectionsData["8"] = valve_connections["8"].toString();
+        valveConnectionsData["9"] = valve_connections["9"].toString();
+        json["valve_connections"] = valveConnectionsData;
 
 
+        // Save absolute maxmiums
+        QJsonObject absoluteMaxData;
+        absoluteMaxData["pressure"] = absoulte_maximums["pressure"].toString();
+        absoluteMaxData["value_timing"] = absoulte_maximums["value_timing"].toString();
+        absoluteMaxData["vacuum_max"] = absoulte_maximums["vacuum_max"].toString();
+        json["absoulte_maximums"] = absoluteMaxData;
 
-        qDebug() << json;
+
+        // Save polling times
+        QJsonObject pollingData;
+        pollingData["pressure_sensor"] = polling_times["pressure_sensor"].toString();
+        pollingData["vacuum_sensor"] = polling_times["vacuum_sensor"].toString();
+        json["polling_times"] = pollingData;
     }
 
 }}
