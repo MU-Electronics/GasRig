fd013e364602e89c78f3dc1e226ed46613d3ec95
motters <sammottley@gmail.com>
2017-11-27 16:44:53 +0000

    Fixed error with vent state machine

--- app/Experiment/Machines/Vent.cpp
+++ app/Experiment/Machines/Vent.cpp
@@ -33,7 +33,6 @@ namespace App { namespace Experiment { namespace Machines
         ,   sml_waitForPressureAfterMultiPipe(&machine)
         ,   sml_waitForPressureAfterFlowOnePipe(&machine)
         ,   sml_waitForPressureAfterFlowTwoPipe(&machine)
-        ,   sml_waitForPressureAfterExhuast(&machine)
 
         ,   sml_validatePressureAfterSlowExhuast(&machine)
         ,   sml_validatePressureAfterOutput(&machine)
@@ -43,7 +42,6 @@ namespace App { namespace Experiment { namespace Machines
         ,   sml_validatePressureAfterMultiPipe(&machine)
         ,   sml_validatePressureAfterFlowOnePipe(&machine)
         ,   sml_validatePressureAfterFlowTwoPipe(&machine)
-        ,   sml_validatePressureAfterExhuast(&machine)
 
 
             // Close valve related states
@@ -105,7 +103,6 @@ namespace App { namespace Experiment { namespace Machines
         connect(&sml_stageFinder, &QState::entered, this, &Vent::stageFinder);
 
         // Validate pressure for atmospheric
-        connect(&sml_validatePressureAfterExhuast, &Functions::CommandValidatorState::entered, this, &Vent::validatePressureForAtmospheric);
         connect(&sml_validatePressureAfterSlowExhuast, &Functions::CommandValidatorState::entered, this, &Vent::validatePressureForAtmospheric);
         connect(&sml_validatePressureAfterOutput, &Functions::CommandValidatorState::entered, this, &Vent::validatePressureForAtmospheric);
         connect(&sml_validatePressureAfterVacOutput, &Functions::CommandValidatorState::entered, this, &Vent::validatePressureForAtmospheric);
@@ -289,14 +286,7 @@ namespace App { namespace Experiment { namespace Machines
             // Failed close all valves
             sml_validateOpenExhuast.addTransition(this->valves(), &Functions::Valves::emit_validationFailed, &sm_stopAsFailed);
             // Success finish here
-            sml_validateOpenExhuast.addTransition(this->valves(), &Functions::Valves::emit_validationSuccess, &sml_waitForPressureAfterExhuast);
-                // Wait for pressure reading?
-                sml_waitForPressureAfterExhuast.addTransition(&m_hardware, &Hardware::Access::emit_pressureSensorPressure, &sml_validatePressureAfterExhuast);
-                    // Are we at atmopheric
-                    sml_validatePressureAfterExhuast.addTransition(this, &Vent::emit_validationSuccess, &sml_openSlowExhuastPath);
-                    sml_validatePressureAfterExhuast.addTransition(this, &Vent::emit_validationFailed, &sml_waitForPressureAfterExhuast);
-
-
+            sml_validateOpenExhuast.addTransition(this->valves(), &Functions::Valves::emit_validationSuccess, &sml_openSlowExhuastPath);
 
         // Open the slow exhuast valve and wait for pressure drop
         sml_openSlowExhuastPath.addTransition(&m_hardware, &Hardware::Access::emit_setDigitalPort, &sml_validateOpenSlowExhuastPath);
